a02912e869927ab15252acb59cc9835f76138f46
XyvinTech <141544499+XyvinTech@users.noreply.github.com>
2024-05-06 12:45:34 +0530

    test

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,7 +1,7 @@
 import { Box } from '@mui/material'
 import React from 'react'
 import LoginForm from '../components/login/loginForm'
-import logo from '../assets/logo/logo-red-trans.png';
+import logo from '../assets/logo/logo-new.jpeg';
 
 export default function Login() {
     return (
@@ -14,9 +14,9 @@ export default function Login() {
                 backgroundColor: '#f9ced5'
             }}>
             <Box sx={{ borderRadius: '8px',p:4,boxShadow:'0 0 6px #5559', backgroundColor:'#ffffff' }}>
-                <Box sx={{mt:1,mb:5}}>
+                <Box sx={{mt:1}}>
 
-                    <img width={'100%'} height={'50px'} style={{objectFit:'contain'}} src={logo} alt="logo" />
+                    <img width={'100%'} height={'200px'} style={{objectFit:'contain',borderRadius:'50%'}} src={logo} alt="logo" />
                 </Box>
                 <LoginForm />
             </Box>
